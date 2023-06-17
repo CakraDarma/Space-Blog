6e0699193f34e9cf51cc3bf0779da4f2bfff51f5
CakraDarma <baguscakraa02@gmail.com>
2023-06-18 03:16:25 +0800

    add about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,9 +1,43 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const About = () => (
-  <section>
-    About section
-  </section>
+	<section className={`${styles.paddings} relative z-10`}>
+		<div className='gradient-02 z-0' />
+		<motion.div
+			variants={staggerContainer}
+			initial='hidden'
+			whileInView='show'
+			viewport={{ once: true, amount: 0.25 }}
+			className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+		>
+			<TypingText title='About Space Blog' textStyles='text-center' />
+
+			<motion.p
+				variants={fadeIn('up', 'tween', 0.2, 1)}
+				className='mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white'
+			>
+				With the{' '}
+				<span className='font-extrabold text-white'>Space Blog app</span>, you
+				can stay updated with the latest news and discoveries from the world of
+				astronomy. Dive into a rich collection of articles written by leading
+				experts, covering a wide range of topics including celestial bodies,
+				space missions, astronomical phenomena, and scientific breakthroughs
+			</motion.p>
+
+			<motion.img
+				variants={fadeIn('up', 'tween', 0.3, 1)}
+				src='/arrow-down.svg'
+				alt='arrow down'
+				className='w-[18px] h-[28px] object-contain mt-[28px]'
+			/>
+		</motion.div>
+	</section>
 );
 
 export default About;
